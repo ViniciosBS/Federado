821bead91b773c8b2969b092de2cbf33161bc50b
ViniciosB <viny1841@gmail.com>
2023-10-05 09:07:49 -0300

    Atualizar o client.py

--- Federado/client.py
+++ Federado/client.py
@@ -1,1 +1,35 @@
-import flwr
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# Carregar dados
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Pré-processamento
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Definir modelo
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3,3), activation='relu')
+])
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+# Compilar modelo
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Treinar modelo
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
+
+# Avaliar modelo
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print(f'Test accuracy: {test_acc}')
+
